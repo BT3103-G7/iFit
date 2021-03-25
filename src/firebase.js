8d999efa01e7c195e88a70c1076dc49d7be4c03e
nic jh <65008375+nicolejh19@users.noreply.github.com>
2021-03-25 23:59:09 +0800

    Update firebase for authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-import firebase from "firebase"
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBIGrd-UhDZMJPrmvx_q0QYjBhBA5bz0ok",
-    authDomain: "bt3103-test-project-750e2.firebaseapp.com",
-    projectId: "bt3103-test-project-750e2",
-    storageBucket: "bt3103-test-project-750e2.appspot.com",
-    messagingSenderId: "424721779966",
-    appId: "1:424721779966:web:0f51a977b94f58c6f269af",
-    measurementId: "G-7DDRBMTV77"
-  };
-  // Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBIGrd-UhDZMJPrmvx_q0QYjBhBA5bz0ok",
+  authDomain: "bt3103-test-project-750e2.firebaseapp.com",
+  projectId: "bt3103-test-project-750e2",
+  storageBucket: "bt3103-test-project-750e2.appspot.com",
+  messagingSenderId: "424721779966",
+  appId: "1:424721779966:web:0f51a977b94f58c6f269af",
+  measurementId: "G-7DDRBMTV77"
+};
 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 var database = firebase.firestore();
 export default database;
