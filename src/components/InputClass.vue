f1c1f0646e3cfcb9c9655a2dbc000b72dd4d0553
Yi En Ong <yienongg@gmail.com>
2021-04-07 01:59:46 +0800

    Add backend for Input Class page

--- src/components/InputClass.vue
+++ src/components/InputClass.vue
@@ -8,49 +8,20 @@
     <div id="date-activity-placeholder">
       <p class="p-3 mb-2 bg-danger text-white">Choose a date </p>
       <b-form-datepicker id="datepicker-full-width" menu-class="w-100" calendar-width="100%" v-model="date" :max="maxDate" locale="en" class="mb-2"></b-form-datepicker>
-      <!-- <p>Value: {{ new Date(date) }}</p> 
-      <p>Type: {{ typeof date }}</p>  !-->
       <br>
       <p class="p-3 mb-2 bg-danger text-white">Choose a class</p>
       <div>
-          <b-form-select v-model="activity" :options="class_options" class="mb-3">
+          <b-form-select v-model="class_chosen" :options="class_options" class="mb-3">
               <!-- This slot appears above the options from 'options' prop -->
               <template #first>
                   <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
               </template>
           </b-form-select>
-          <!-- <div class="mt-2">Value: <strong>{{ activity }}</strong></div> -->
       </div>
     </div>
     <br><br><br>
 
-<!--     <div id="time-placeholder">
-        <div style='width:20%; height:100%; float:left'><br></div>
-        <div style='width:20%; height:100%; float:left'>
-            <p class="p-3 mb-2 bg-danger text-white">Start time</p>
-            <div> -->
-                <!-- <b-form-timepicker v-model="start" locale="en"></b-form-timepicker> -->
-                <!-- <div class="mt-2">Value: {{ start }}</div> -->
-                <!-- <p>Type: {{ Timestamp.valueof(start) }}</p> -->
-            <!-- </div> --> 
-<!--               <b-form-timepicker v-model="start" locale="en"></b-form-timepicker>
-            <br>
-        </div>
-        <div style='width:20%; height:100%; float:left'><br></div>
-          
-        <div style='width:20%; height:100%; float:left'>
-            <p class="p-3 mb-2 bg-danger text-white">End time</p>
-            <div>
-                <b-form-timepicker v-model="end" locale="en"></b-form-timepicker>
-                <div class="mt-2">Value: {{ end }}</div> 
-             </div>
-            <br>
-        </div>
-        <div style='width:20%; height:100%; float:left'><br></div> 
-    </div> -->
-
     <div id="button-placeholder">
-        <!-- <b-button type="submit" variant="warning" size=lg v-on:click="onSubmit()">SUBMIT</b-button> -->
         <br><b-button type="submit" variant="warning" size=lg><b>SUBMIT</b></b-button>
     </div> 
     
@@ -70,11 +41,12 @@ import database from '../firebase'
 
       return {
         maxDate: maxDate,
-        activity: null,
+        class_chosen: null,
         date: null,
-        start: null,
-        end: null,
+        //start: null,
+        //end: null,
         space: ' ',
+        add_seconds: ':00',
         userid: '123432',
         class_options: [],
         calories_list:[]
@@ -82,54 +54,48 @@ import database from '../firebase'
     },
     methods: {
         input() {
-          if (this.activity == null || this.date == null || this.start == null || this.end == null) {// if any field is missing
+          if (this.class_chosen == null || this.date == null) {// if any field is missing
             alert("Please input all fields!")
-          } 
-          else { //if start time is later than end time
+          } else { 
             var fullDate = new Date(this.date)
             var year = fullDate.getFullYear()
             var month = fullDate.getMonth() + 1
             var date = fullDate.getDate()
-            var startTime = new Date(this.date.concat(this.space).concat(this.start))
-            var startHour = startTime.getHours()
-            var endTime = new Date(this.date.concat(this.space).concat(this.end))
-            var endHour = endTime.getHours()
-            var time_difference = (endTime.getTime() - startTime.getTime())/60000 //no. of minutes of activity
-            //console.log(time_difference)
-            if (time_difference <= 0) {
-              alert("End time should be later than start time!")
-            } else {
-              //if(this.activity != null && this.date != null && this.start != null && this.end != null) {
-              var calories = 0
-              var activity = this.activity
-              this.calories_list.forEach(function(test) {
-                console.log(test['name'])
-                console.log(activity)
-                if (activity == test['name']) {
-                  calories = time_difference * test["calspermin"]
-                  console.log(calories)
-                } else {
-                  console.log("DIDNT CALC CALORIES")
-                }
-              });
-              database.collection('input_class(test)').add({
-                  'activity': this.activity,
-                  'calories': Number(calories),
-                  'date': Number(date),
-                  'month': Number(month),
-                  'year': Number(year),
-                  'startHour': Number(startHour),
-                  'endHour': Number(endHour),
-                  'userid': Number(this.userid),
+
+            var calories = 0
+            var startHour = 0
+            var endHour = 0
+            
+            var class_chosen = this.class_chosen
+        
+            this.calories_list.forEach(function(test) {
+              //console.log(test['name'])
+              //console.log(class_chosen)
+              if (class_chosen == test['name']) {
+                calories = test["cal"]
+                startHour = test["start"].slice(0,2)
+                endHour = test["end"].slice(0,2)
+                //console.log(calories)
+              } 
+            });
+            database.collection('inputs').add({
+              'activity': this.class_chosen,
+              'calories': Number(calories),
+              'date': Number(date),
+              'month': Number(month),
+              'year': Number(year),
+              'startHour': Number(startHour),
+              'endHour': Number(endHour),
+              'userid': Number(this.userid),
             })
             .then(() => {
-                        alert('Your activity is recorded!');
+                        alert('Your class is recorded!');
                         this.$router.push('/overview');
                     })
                     .catch(error => {
                         alert(error.message);
                     });
-          }
+
         }
       },
       fetchItems: function() {
@@ -139,7 +105,7 @@ import database from '../firebase'
           snapshot.docs.forEach(doc => {
               classname = {value: doc.data()["name"], text: doc.data()["name"]}; 
               this.class_options.push(classname);
-              calspermin = {'name': doc.data()["name"], 'cal': doc.data()["cal"]}; 
+              calspermin = {'name': doc.data()["name"], 'cal': doc.data()["cal"], 'start': doc.data()["start"], 'end': doc.data()["end"]}; 
               this.calories_list.push(calspermin)
           });
         });
